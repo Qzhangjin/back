2dc437302f2e7f5ac87008142cfc1d659ca2c0bc
Qzhangjin <365445087@qq.com>
2019-04-03 10:32:56 +0800

    增加了ssm框架和密码加密

---
+++ src/main/java/com/neuedu/util/MyPropertyPlaceholderConfigurer.java
@@ -0,0 +1,16 @@
+package com.neuedu.util;
+
+import org.springframework.beans.factory.config.PropertyPlaceholderConfigurer;
+
+/**
+ * Created by Administrator on 2019/4/2.
+ */
+public class MyPropertyPlaceholderConfigurer extends PropertyPlaceholderConfigurer{
+    @Override
+    protected String convertProperty(String propertyName, String propertyValue) {
+        if(propertyName.equals("username")||propertyName.equals("password")) {
+            return DESUTIL.decode(propertyValue);
+        }
+        return propertyValue;
+    }
+}
